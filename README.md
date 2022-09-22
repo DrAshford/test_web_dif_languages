3968c7831dbe31f416132b1618452128aaa38b3a
drashford <dimonchic61@gmail.com>
2022-09-22 22:41:16 +0300

    add_language_param

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # test_web_dif_languages
+Creating tests for testing website in different languages.
--- test_items.py
+++ test_items.py
@@ -5,7 +5,8 @@ from selenium.webdriver.common.by import By
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
-def test_web_ru(browser):
+def test_add_button(browser):
     browser.get(link)
     #time.sleep(600)
-    browser.find_element(By.CSS_SELECTOR, 'button.btn-add-to-basket')
+    button = browser.find_elements(By.CSS_SELECTOR, 'button.btn-add-to-basket')
+    assert len(button) == 1, f"Assertion error: found {len(button)} elements, expected 1!"
